01fb809b8040257a2e3514aeaaf1ec8f2a0d1c79
Dizz7 <162482518+Dizz7@users.noreply.github.com>
2026-08-26 09:25:24 -0400

    Spring initialized

---
+++ micro_a/src/main/java/cl/duoc/micro_a/MicroAApplication.java
@@ -0,0 +1,13 @@
+package cl.duoc.micro_a;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroAApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroAApplication.class, args);
+	}
+
+}
---
+++ micro_a/src/test/java/cl/duoc/micro_a/MicroAApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.duoc.micro_a;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroAApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
