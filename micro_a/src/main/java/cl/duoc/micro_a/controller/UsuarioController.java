2fb3b2169f21a017ee7fddbfc0a25811e8475869
Dizz7 <162482518+Dizz7@users.noreply.github.com>
2026-09-15 10:01:14 -0300

    Update UsuarioController.java
    Se actualiza el controlador de usuarios.

--- micro_a/src/main/java/cl/duoc/micro_a/controller/UsuarioController.java
+++ micro_a/src/main/java/cl/duoc/micro_a/controller/UsuarioController.java
@@ -1,5 +1,8 @@
 package cl.duoc.micro_a.controller;
 
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,31 +12,59 @@ import cl.duoc.micro_a.model.Usuario;
 import cl.duoc.micro_a.service.UsuarioService;
 
 import java.util.List;
+import java.util.Optional;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PutMapping;
+
+
 
 // Controlador para manejar las solicitudes relacionadas con los usuarios
 
+
 @RestController
-@RequestMapping("/usuarios") // Ruta base para las solicitudes de usuarios
+@RequestMapping ("/usuarios")
 public class UsuarioController {
-    
 
+
+
+    @Autowired
     // Inyección de dependencias del servicio de usuarios
-    private final UsuarioService usuarioService;
-
+    private UsuarioService usuarioService;
+ 
     // Constructor para la inyección de dependencias
     public UsuarioController(UsuarioService usuarioService) {
         this.usuarioService = usuarioService;
     }
-
     // Endpoint GET para obtener todos los usuarios
-    @GetMapping
-    public List<Usuario> listarUsuarios() {
+    @GetMapping({"", "/"})
+    public List<Usuario> getUsuarios() {
         return usuarioService.getAllUsuarios();
     }
 
     // Endpoint GET para buscar un usuario por su ID
     @GetMapping("/{id}")
-    public Usuario buscarPorId(@PathVariable int id) {
+    public Optional<Usuario> getUsuarioById(@PathVariable Long id) {
         return usuarioService.getUsuarioById(id);
     }
+
+    // Controlador para crear, actualizar y eliminar usuarios
+    @PostMapping
+    public Usuario createUsuario(@RequestBody Usuario usuario) {
+        return usuarioService.createUsuario(usuario);
+    }
+    
+    @PutMapping("/{id}")
+    public Usuario updateUsuario(@PathVariable Long id, @RequestBody Usuario usuario) {
+        return usuarioService.updateUsuario(id, usuario);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteUsuario(@PathVariable Long id) {
+        usuarioService.deleteUsuario(id);
+    }
+
 }
+    
+
+    
