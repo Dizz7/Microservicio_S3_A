599aa0ffe527a3df8afe2e2dfb34811a80b068e8
Dizz7 <162482518+Dizz7@users.noreply.github.com>
2026-09-15 20:49:08 -0300

    Validaciones y manejo de excepciones
    Se agregan validaciones a los campos de la tabla usuario y también se agrega manejo de excepciones.

--- micro_a/src/main/java/cl/duoc/micro_a/model/Usuario.java
+++ micro_a/src/main/java/cl/duoc/micro_a/model/Usuario.java
@@ -6,6 +6,9 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Table;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 
 
 
@@ -19,10 +22,28 @@ public class Usuario {
     @Column(name = "id")
     private Long id;
 
+
+    // Validaciones de los campos de la tabla usuario
+    @Pattern (regexp = "^[a-zA-Z0-9]+$", message = "El nombre de usuario solo puede contener letras y números")
+    @Size (min = 3, max = 20, message = "El nombre de usuario debe tener entre 3 y 20 caracteres")
+    @NotBlank (message = "El nombre de usuario no puede estar vacío")
     @Column(name = "n_usuario")
     private String n_usuario;
+    
+    // Rol
+    @Pattern (regexp = "^[a-zA-Z]+$", message = "El rol solo puede contener letras y números")
+    @Size (min = 3, max = 20, message = "El rol debe tener entre 3 y 20 caracteres")
+    @NotBlank (message = "El rol no puede estar vacío")
     @Column(name = "rol")
     private String rol;
+
+
+    // Dirección
+    @Pattern(
+    regexp = "^[a-zA-Z0-9. ]+$",
+    message = "La dirección solo puede contener letras, números, puntos y espacios")
+    @Size (min = 3, max = 40, message = "La dirección debe tener entre 3 y 40 caracteres")
+    @NotBlank (message = "La dirección no puede estar vacía")
     @Column(name = "direccion")
     private String direccion;
 
