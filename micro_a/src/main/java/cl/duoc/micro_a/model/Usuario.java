ebbfff5a5c1abef4058045270942fff296c0b15f
Dizz7 <162482518+Dizz7@users.noreply.github.com>
2026-09-15 09:42:55 -0300

    Update Usuario.java y Repository

--- micro_a/src/main/java/cl/duoc/micro_a/model/Usuario.java
+++ micro_a/src/main/java/cl/duoc/micro_a/model/Usuario.java
@@ -1,58 +1,63 @@
 package cl.duoc.micro_a.model;
 
-/**
- * Clase que representa un usuario en el sistema.
- * Sin @Entity ni @Table porque no se está usando base de datos aún.
- */
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Table;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 
+
+
+
+@Entity
+@Table(name = "usuario")
 public class Usuario {
+    @Id
 
-    private int id; // ID del Usuario
-    private String n_usuario; // Nombre de Usuario
-    private String rol; // Rol del Usuario
-    private String direccion; // Dirección de despacho del Usuario
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "n_usuario")
+    private String n_usuario;
+    @Column(name = "rol")
+    private String rol;
+    @Column(name = "director")
+    private String direccion;
+
+
+
+
+    // Constructor vacío requerido por JPA
+    public Usuario() {
+    }
+
+
 
     // Constructor
 
-    public Usuario(int id,String n_usuario, String rol, String direccion) {
+    public Usuario(Long id,String n_usuario, String rol, String direccion) {
         this.id = id;
         this.n_usuario = n_usuario;
         this.rol = rol;
         this.direccion = direccion;
     }
 
-    // Getters and Setters
-    public int getId() {
-            return id;
-    }
+    // Getters y Setters
+    
+    public Long getId() { return id;}
+    public void setId(Long id) { this.id = id;}
 
-    public void setId(int id) {
-            this.id = id;
-    }
+    public String getN_usuario() { return n_usuario;}
+    public void setN_usuario(String n_usuario) { this.n_usuario = n_usuario; }
 
-    public String getN_usuario() {
-        return n_usuario;
-    }
+    public String getRol() { return rol;}
+    public void setRol(String rol) { this.rol = rol;}
 
-    public void setN_usuario(String n_usuario) {
-        this.n_usuario = n_usuario;
-    }
-
-    public String getRol() {
-        return rol;
-    }
-
-    public void setRol(String rol) {
-        this.rol = rol;
-    }
-
-    public String getDireccion() {
-        return direccion;
-    }
-
-    public void setDireccion(String direccion) {
-        this.direccion = direccion;
-    }
-
+    public String getDireccion() { return direccion;}
+    public void setDireccion(String direccion) { this.direccion = direccion;}
 
 }
---
+++ micro_a/src/main/java/cl/duoc/micro_a/repository/UsuarioRepository.java
@@ -0,0 +1,11 @@
+package cl.duoc.micro_a.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+// Se importó el modelo Pelicula para poder usarlo en el repositorio
+import cl.duoc.micro_a.model.Usuario;
+
+
+// Se extiende de JpaRepository para poder usar los métodos de JPA y poder hacer consultas a la base de datos
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+
+    }
